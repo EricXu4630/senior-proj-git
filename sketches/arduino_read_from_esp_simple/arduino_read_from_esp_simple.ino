d131989bb7e67cbe66a837a082300a03ed851e39
Eric Xu <eric.xu@cooper.edu>
2023-02-22 19:55:00 -0500

    Update arduino_read_from_esp_simple.ino

--- sketches/arduino_read_from_esp_simple/arduino_read_from_esp_simple.ino
+++ sketches/arduino_read_from_esp_simple/arduino_read_from_esp_simple.ino
@@ -32,16 +32,12 @@ void update_pos(){
   float a = output2;
   float b = output1;
   float cos_a = (b*b + c*c - a*a) / (2*b*c);
-  float y_val = b * sqrt(1 - (cos_a * cos_a));
-  if (!isnan(cos_a)){
-    v.x = b * cos_a;
-  }
-  if (!isnan(y_val)){
-    v.y = y_val;
-  }
-  Serial.print(a); 
-  Serial.print(","); 
-  Serial.print(b); 
+  v.x = b * cos_a;  
+  v.y = b * sqrt(1 - (cos_a * cos_a));
+
+  Serial.print(v.x); 
+  Serial.print(", "); 
+  Serial.print(v.y); 
   Serial.println(); 
   return v; 
 }
@@ -49,52 +45,46 @@ void update_pos(){
 void read_serial() {
   // put your main code here, to run repeatedly:
   if(Serial3.available()){
-      input_stack1[counter1++] = Serial3.read(); 
+    input_stack1[counter1++] = Serial3.read(); 
   }
   if(Serial2.available()){
-      input_stack2[counter2++] = Serial2.read(); 
-  }
-  if (counter1 == 4){
-      memcpy(&output1, input_stack1, sizeof(float));
-      // Serial.print("Output1: ");
-      // Serial.println(output1);
-      counter1 = 0;
-      if(output1>0.01 && output1<10.0 && output2>0.01 && output2<10.0){
-        update_pos();
-      }
-  }
-  if (counter2 == 4){
-      memcpy(&output2, input_stack2, sizeof(float));
-      // Serial.print("Output2: ");
-      // Serial.println(output2);
-      counter2 = 0;  
-      if(output1>0.01 && output1<10.0 && output2>0.01 && output2<10.0){
-        update_pos();
-      }   
+    input_stack2[counter2++] = Serial2.read(); 
   }
 
+  if (counter1 == 4){
+    memcpy(&output1, input_stack1, sizeof(float));
+    // Serial.print("Output1: ");
+    // Serial.println(output1);
+    counter1 = 0;
+  }
+  if (counter2 == 4){
+    memcpy(&output2, input_stack2, sizeof(float));
+    // Serial.print("Output2: ");
+    // Serial.println(output2);
+    counter2 = 0;        
+  }
+
+  if(output1>0.00 && output1<10.0 && output2>0.00 && output2<10.0){
+    update_pos();
+  }
 }
 
 void turn_left() {
   // Serial.println("left"); 
-  roboclaw.ForwardM2(address, 30); 
+  // roboclaw.ForwardM2(address, 20); 
+  roboclaw.TurnLeftMixed(address, 20); 
 }
 
 void turn_right() {
   // Serial.println("right"); 
-  roboclaw.ForwardM1(address, 30); 
+  roboclaw.TurnRightMixed(address, 20); 
 }
 
 void forward() {
   // Serial.println(" forward"); 
-  roboclaw.ForwardM1(address, 30) && roboclaw.ForwardM2(address, 30); 
+  roboclaw.ForwardMixed(address, 30); 
 }
 
-void forwardSlow() {
-  // Serial.println("going forward"); 
-  roboclaw.ForwardM1(address, 10); 
-  roboclaw.ForwardM2(address, 10); 
-}
 
 void stop () {
   // Serial.println("stopping"); 
@@ -105,15 +95,22 @@ void stop () {
 
 void loop () {
   read_serial(); 
-  // if (v.y > 1.0){
-  //   return forward();
-  // }
-  if (v.x < -0.1){
-     return turn_right();
-  } 
-  if (v.x > 0.6){
-     return turn_left();
-  } 
-  stop();
-  delay(300);
+  if (!isnan(v.x) && !isnan(v.y)){
+    if (v.y > 1.0 && v.y < 10.0){
+      if (v.x > 0.52 && v.x < 5.0){
+        return turn_right();
+      }
+      else if (v.x < 0 && v.x > -4.5){
+        return turn_left();
+      }
+      else if (v.x >= 0.0 && v.x <= 0.5){
+        return go_forward();
+      }    
+    }
+    else if (v.y <=1.0){
+      return stop;
+    }
+  }
+
+  delay(500);
 }
