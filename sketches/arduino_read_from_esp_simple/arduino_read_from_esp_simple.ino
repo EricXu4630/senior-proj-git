125297a3ba75bdffc969fc7a95a02051f3bd2ba0
Eric Xu <eric.xu@cooper.edu>
2023-03-07 17:50:21 -0500

    Update arduino_read_from_esp_simple.ino

--- sketches/arduino_read_from_esp_simple/arduino_read_from_esp_simple.ino
+++ sketches/arduino_read_from_esp_simple/arduino_read_from_esp_simple.ino
@@ -2,7 +2,6 @@
 #include <SoftwareSerial.h>
 #include "SerialTransfer.h"
 
-
 SoftwareSerial serial(10,11); 
 RoboClaw roboclaw(&serial, 10000); 
 
@@ -14,13 +13,6 @@ SerialTransfer anchor2_transfer;
 float output1; 
 float output2; 
 
-struct Vec {
-  float x; 
-  float y; 
-} v;
-
-unsigned long starttime;
-unsigned long endtime;
 
 void setup() {
   // put your setup code here, to run once:
@@ -30,17 +22,6 @@ void setup() {
   Serial2.begin(115200);
   anchor2_transfer.begin(Serial2); 
   roboclaw.begin(38400); 
-}
-
-void update_pos(){
-  // CHECK FOLLOWING
-  const float c = 0.495;
-  float b = output1;
-  float a = output2;
-  float cos_a = (b*b + c*c - a*a) / (2*b*c);
-  v.x = b * cos_a;  
-  v.y = b * sqrt(1 - (cos_a * cos_a));
-
 }
 
 void read_serial() {
@@ -68,64 +49,33 @@ void read_serial() {
 }
 
 void turn_left() {
-  // Serial.println("left"); 
   roboclaw.ForwardM1(address, 30); 
   roboclaw.ForwardM2(address, 0); 
   // roboclaw.TurnLeftMixed(address, 40); 
 }
 
 void turn_right() {
-  // Serial.println("right"); 
   roboclaw.ForwardM1(address, 0);
   roboclaw.ForwardM2(address, 30); 
   // roboclaw.TurnRightMixed(address, 40); 
 }
 
 void forward(float speed) {
-  // Serial.println("forward"); 
   roboclaw.ForwardM1(address, speed);
   roboclaw.ForwardM2(address, speed); 
   // roboclaw.ForwardMixed(address, speed); 
 }
 
 void stop() {
-  // Serial.println("stopping"); 
   roboclaw.ForwardM1(address, 0);
   roboclaw.ForwardM2(address, 0); 
-  // roboclaw.ForwardMixed(address, 0); 
 }
 
 void loop () {
   read_serial();
-  // starttime = millis();
-  // endtime = starttime;
-  // while ((endtime - starttime) <=1000) // do this loop for up to 1000ms
-  // {
-  // if (!isnan(v.x) && !isnan(v.y)){
-  //   if (v.y > 0.5 && v.y < 10.0){
-  //     if (v.x > 0.52 && v.x < 5.0){
-  //       return turn_right();
-  //     }
-  //     else if (v.x < 0 && v.x > -4.5){
-  //       return turn_left();
-  //     }
-  //     else if (v.x >= 0.0 && v.x <= 0.52){
-  //       return forward();
-  //     }    
-  //   }
-  //   else if (v.y <=0.5){
-  //     return stop;
-  //   }
-  // }
-
 
   delay(random(50, 100));
   float avg = (output1+output2)/2;
-
-  // starttime = millis();
-  // endtime = starttime;
-  // while ((endtime - starttime) <= 1000) // do this loop for up to 700ms
-  // {
 
   if (avg>=0 && avg<4.0 && !isnan(output1) && !isnan(output2)){
     if (avg > 0.5){ 
@@ -139,18 +89,13 @@ void loop () {
       }
       else{
         delay(10);
-        forward(15+(avg*15));
+        forward(15+(avg*25));
       }    
     }
     else{
       stop();
     }
   }
-
-
-  //   endtime = millis();
-  // }
-
 }
 
 
